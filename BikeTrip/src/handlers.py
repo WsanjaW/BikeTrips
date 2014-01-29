16a73dd7cb17233d912358619635dc6f7ceab78f
WsanjaW <sanjak_5@hotmail.com>
2014-01-29 04:19:17 +0100

    OneTrip handler added. Handlers for uploading and downloading files added

--- BikeTrip/src/handlers.py
+++ BikeTrip/src/handlers.py
@@ -7,13 +7,17 @@ import os
 import webapp2
 import jinja2
 import json
+import urllib
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
 from google.appengine.api import urlfetch
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.ext.blobstore import BlobInfo
 
 from datetime import datetime
-from models import Trip
+from models import Trip,Track
 
 
 #set jinja templates environment
@@ -138,6 +142,7 @@ class ImageHandler (webapp2.RequestHandler):
 
 class CityInfo(webapp2.RequestHandler):
     '''
+    Shows city on google map using geonames.org to get coordinates
     '''
     def proces(self,json_string):
         '''
@@ -183,10 +188,83 @@ class CityInfo(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('templates/cities.html')
         self.response.write(template.render(template_values))
     
+class OneTrip(webapp2.RequestHandler):
+    '''
+    Shows one trip, with id sent in url, and tracks for that trip
+    Shows form for uploading gpx files
+    '''
+    def get(self):
+        
+        upload_url = blobstore.create_upload_url('/upload')
+
+        user = users.get_current_user()
+        id ='0'
+        #check if user is logged in and creates appropriate url
+        if user:
+            id = user.user_id()
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+        
+        #get specific trip     
+        trip_key = self.request.get('trip_id')
+        tripk = ndb.Key(urlsafe=trip_key)
+        trip = tripk.get()
+        
+        #get tracks for trip
+        track_query = Track.query(ancestor=tripk)
+        tracks = track_query.fetch(10)
+        #get number of tracks
+        num = len(tracks)
+        
+        #get blobInfo objects from blob_key
+        bli = []
+        for track in tracks:
+            bli.append(BlobInfo(track.blob_key))
+        #create template
+        template_values = {'user': user, 'url': url, 'url_linktext': url_linktext,'trip':trip,'upload':upload_url,'tracks':tracks,'blobs':bli,'num':num}
+        template = JINJA_ENVIRONMENT.get_template('templates/onetrip.html')
+        
+        #set cookie value to this page url
+        self.response.set_cookie('redirect_url', self.request.url)
+
+        self.response.write(template.render(template_values))
     
-    
-    
-    
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    '''
+    Handler for uploading gpx file to blobstore
+    '''
+    def post(self):
+        
+        upload_files = self.get_uploads('file') 
+        blob_info = upload_files[0]
+        #get trip_id from hidden filed in form
+        trip_key = self.request.get('trip_id')
+        tripk = ndb.Key(urlsafe=trip_key)
+        #create new track which parent is current track 
+        track = Track(parent=tripk,track_name=self.request.get('trip_name'),blob_key=blob_info.key())   
+        track.put()
+        
+        #get cookie with value of page from where upload is started
+        cookie_value = self.request.cookies.get('redirect_url')
+        self.redirect(str(cookie_value))
+
+
+class DownloadHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    '''
+    Handles file download
+    '''
+    def get(self, resource):
+        
+       
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        save_as_name =  blob_info.filename
+        mime_type='text/xml'
+        self.send_blob(blob_info,content_type=mime_type,save_as=save_as_name)
+
     
     
     

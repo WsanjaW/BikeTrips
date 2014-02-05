672803a7dab5da6368b8ad77814b5912f26cec4f
WsanjaW <sanjak_5@hotmail.com>
2014-02-05 23:08:05 +0100

    Size of file validation added.

--- BikeTrip/src/static/js/addFormValidation.js
+++ BikeTrip/src/static/js/addFormValidation.js
@@ -11,6 +11,12 @@ $(document).ready(function(){
      			}
 				return false;
 			}, "false");
+     		$.validator.addMethod('filesize', function(value, element, param) {
+     		    // param = size (en bytes) 
+     		    // element = element to validate (<input>)
+     		    // value = value of the element (file name)
+     		    return this.optional(element) || (element.files[0].size <= param) 
+     		});
      		$('#addform').validate(
      	    		{
      	    			rules: {
@@ -28,7 +34,8 @@ $(document).ready(function(){
      	    				},
      	    				img: {
      	    				
-     	    					accept: "image/*",
+     	    					filesize: 1048576,
+     	    					accept: "image/*"
      	    					
      	    				},
      	    				cities: {
@@ -44,7 +51,7 @@ $(document).ready(function(){
      	            			minlength: jQuery.format("Enter at least {0} characters"),
      	            		},
      	            		img: {
-     	    				
+     	            			filesize: "Image too large",
      	    					accept: "File must be image"
      	    					
      	    				},

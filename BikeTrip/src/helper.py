e9eaaddb4169c99ce49570d7fbf083c6614134a4
WsanjaW <sanjak_5@hotmail.com>
2014-02-14 02:27:08 +0100

    calculate_trip_stat method added

--- BikeTrip/src/helper.py
+++ BikeTrip/src/helper.py
@@ -3,7 +3,7 @@ Created on 13.02.2014.
 
 @author: Sanja
 '''
-
+from models import Trip,Track,TrackStatistic
    
 def sec_to_time(time):
     '''
@@ -27,7 +27,51 @@ def time_to_sec(time):
     h = int(t[0])
     m = int(t[1])
     s = int(t[2])
-    return h * 3600 + m * 60 + s 
+    return h * 3600 + m * 60 + s
+
+def calculate_trip_stat(trip_key):
+    '''
+    Calculates statistic for whole trip
+    '''
+    #get tracks for trip
+    track_query = Track.query(ancestor=trip_key)
+    tracks = track_query.fetch(20)
+    stats = []
+    #get statistic from every track
+    for track in tracks:
+        track_stat_query= TrackStatistic.query(ancestor=track.key)
+        track_stat = track_stat_query.fetch(1)
+        if  track_stat != []:
+            stats.append(track_stat[0])
+   
+    #get trip 
+    trip= trip_key.get()
+  
+    #calculate total distance
+    dist = sum([s.total_distance for s in stats])
+    trip.trip_statistic.total_distance = dist
+    
+    #calculate total time
+    time_sec = sum([time_to_sec(s.total_time) for s in stats])
+    time = sec_to_time(time_sec)
+    trip.trip_statistic.total_time = time
+    
+    #calculate avr
+    if time_sec > 0:
+        trip.trip_statistic.avr_speed = dist/(time_sec * 0.000277778)
+    else:
+        trip.trip_statistic.avr_speed = 0
+   
+    #calculate total climb
+    climb = sum([s.total_climb for s in stats])
+    trip.trip_statistic.total_climb = climb
+    
+    #calculate max elev
+    maxel = max([s.max_elev for s in stats] + [-100])
+    trip.trip_statistic.max_elev = maxel
+    
+    trip.put()
+     
 
   
         

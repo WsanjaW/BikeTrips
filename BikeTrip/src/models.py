cdbf41eb990b71e4afcab25421b6cca2cbd3d782
WsanjaW <sanjak_5@hotmail.com>
2014-02-14 02:26:46 +0100

    Model updated

--- BikeTrip/src/models.py
+++ BikeTrip/src/models.py
@@ -7,21 +7,6 @@ Created on 23.01.2014.
 from google.appengine.ext import ndb
 from google.appengine.ext import blobstore
 
-class Trip(ndb.Model):
-    '''
-    Models a Trip with name,creation date, from date, to date, description and trip avatar, cities and visibility.
-    '''
-    
-    trip_name = ndb.StringProperty()
-    creation_date = ndb.DateTimeProperty(auto_now_add=True)
-    from_date = ndb.DateProperty();
-    to_date = ndb.DateProperty();
-    trip_avatar = ndb.BlobProperty()
-    description = ndb.TextProperty()
-    cities = ndb.StringProperty(repeated=True)
-    visibility = ndb.BooleanProperty()
-    
-
 class Track(ndb.Model):
     '''
     Models a Track
@@ -35,15 +20,36 @@ class Track(ndb.Model):
 class TrackStatistic(ndb.Model):
     '''
     Models Statistic.
-    *Has reference to Track to provide one-to-many relationship
+    ****CHANGED to parent model to ensure consistency**** Has reference to Track to provide one-to-many relationship****
+    
     *TrackStatistic dosen't belongs to same group as corresponding track
     *total_time is StringProperty because it measure duration, so no other formats were appropriate
     '''
-    track = ndb.KeyProperty(kind=Track)
+#     track = ndb.KeyProperty(kind=Track)
     name = ndb.StringProperty()
     total_distance = ndb.FloatProperty()
     total_time = ndb.StringProperty()
     avr_speed = ndb.FloatProperty()
     total_climb = ndb.FloatProperty()
     max_elev = ndb.FloatProperty()
+    
+class Trip(ndb.Model):
+    '''
+    Models a Trip with name,creation date, from date, to date, description and trip avatar, cities and visibility.
+    '''
+    
+    trip_name = ndb.StringProperty()
+    creation_date = ndb.DateTimeProperty(auto_now_add=True)
+    from_date = ndb.DateProperty();
+    to_date = ndb.DateProperty();
+    trip_avatar = ndb.BlobProperty()
+    description = ndb.TextProperty()
+    cities = ndb.StringProperty(repeated=True)
+    visibility = ndb.BooleanProperty()
+    #for trip statistic
+    trip_statistic = ndb.StructuredProperty(TrackStatistic)
+
+    
+
+
     
